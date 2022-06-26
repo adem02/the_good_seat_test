bedf409739bcfa531953ffca84795b616a819760
dem <demahmed02@gmail.com>
2022-06-27 01:09:30 +0200

    End of the refactoring exercice

--- exo2/api/dto/response.transformer.ts
+++ exo2/api/dto/response.transformer.ts
@@ -2,7 +2,6 @@ import { IUser } from "../models/user.model";
 
 export class ResponseTransformer {
     static user(user: IUser): any {
-        // TODO transform input
         return user;
     }
 }
